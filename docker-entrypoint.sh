55683ff9c670a922e2148db6cee112a94c0b1963
GTPE Student <fredrik.tarnell-hpe.com@clientvm.2725.internal>
2019-04-12 09:44:36 +0000

    Adding support for user uid in docker-entry script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 echo Starting SSH session as ssh -l ${SSH_USER} ${SSH_HOST}
 ssh -l ${SSH_USER} ${SSH_HOST}
 echo SSH exited, sleeping for an hour
