a90e8fa2dd7bdfb82b20d6c12842c25bc00b0782
Fredrik Tarnell <frta@localhost.localdomain>
2019-04-14 21:46:32 +0200

    ssh user fixed in config file

--- create-using-newapp.sh
+++ create-using-newapp.sh
@@ -5,7 +5,7 @@ oc new-app https://github.com/frippe75/ssh-forward.git \
     -e SSH_HOST=hpe.rre.nu
 
 #https://www.jannikarndt.de/blog/2018/03/ssh_key_as_kubernetes_secret/
-kubectl create secret generic my-ssh-key --from-file=id_rsa=.key/key.pem
+kubectl create secret generic my-ssh-key --from-file=id_rsa=key/key.pem
 
 
 
